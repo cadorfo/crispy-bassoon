4dbfaeff6f1bf7dad87205c2cd0652d47847a28f
Carlos Nascimento <carlos.nascimento@adtsys.com.br>
2017-05-20 13:15:55 -0300

    Mavem and spring boot stuff

---
+++ src/main/java/com/carlos/weblanches/WeblanchesApplication.java
@@ -0,0 +1,12 @@
+package com.carlos.weblanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblanchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblanchesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/carlos/weblanches/models/SandwichTest.java
@@ -0,0 +1,62 @@
+package com.carlos.weblanches.models;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.carlos.weblanches.models.SandwichEnum.*;
+
+/**
+ * Created by carlos on 20/05/17.
+ */
+public class SandwichTest {
+
+    @Test
+    public void cost() {
+        List<Ingredient> ingredientList = new ArrayList<>();
+        ingredientList.add(IngredientEnum.BACON.getIngrediment());
+        ingredientList.add(IngredientEnum.HAMBURGER.getIngrediment());
+        Sandwich sandwich = new Sandwich(ingredientList);
+        double totalCost = calculateIngredientsCost(ingredientList);
+        assert totalCost == sandwich.getTotalCost();
+    }
+
+    @Test
+    public void costXEGGCost() {
+
+        Sandwich sandwich = new Sandwich(XEGG.getIngredientList());
+        double totalCost = calculateIngredientsCost(XEGG.getIngredientList());
+        assert totalCost == sandwich.getTotalCost();
+    }
+
+    @Test
+    public void costXEGGBaconCost() {
+
+        Sandwich sandwich = new Sandwich(XEGGBACON.getIngredientList());
+        double totalCost = calculateIngredientsCost(XEGGBACON.getIngredientList());
+        assert totalCost == sandwich.getTotalCost();
+    }
+
+    @Test
+    public void costBaconCost() {
+
+        Sandwich sandwich = new Sandwich(XBACON.getIngredientList());
+        double totalCost = calculateIngredientsCost(XBACON.getIngredientList());
+        assert totalCost == sandwich.getTotalCost();
+    }
+
+    @Test
+    public void costBurgerCost() {
+
+        Sandwich sandwich = new Sandwich(XBURGER.getIngredientList());
+        double totalCost = calculateIngredientsCost(XBURGER.getIngredientList());
+        assert totalCost == sandwich.getTotalCost();
+    }
+
+
+    private double calculateIngredientsCost(List<Ingredient> ingredients) {
+        return ingredients.stream().collect(Collectors.summingDouble(item -> item.getCost()));
+    }
+}
